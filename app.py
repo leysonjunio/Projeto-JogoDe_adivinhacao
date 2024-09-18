728836e0463148e8a2075daf28b0251ad5cb8577
Leyson junio <148728893+leysonjunio@users.noreply.github.com>
2024-09-18 20:53:40 -0300

    ATZ app.py

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import random
 numero_secreto = random.randint(1 , 100) #função para gerar numero aleatorio
 while True: #Aqui inicio o loop até que a condição seja verdadeira (Ou seja que jogador acerte o número secreto).
     chute = int(input("Digite um número entre 1 e 100: "))#O meu INPUT vai pedir o jogador para inserir um valor inteiro(str) no console a minha função INT convertera a string para número inteiro e guarda na variavel CHUTE
+    if chute < numero_secreto:
+        print("O seu chute foi menor do que o número secreto.")
+    elif chute > numero_secreto:
+        print("O seu chute foi maior do que o número secreto.")
+    else:
+        print("Parabéns! Você acertou!")
+        break
