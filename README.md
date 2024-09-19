4fc72b61640a0359c0f34b3538965d64d196a5c1
Leyson junio <148728893+leysonjunio@users.noreply.github.com>
2024-09-19 20:32:02 -0300

    atz

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 
     Inicio do projeto dia 18/09
 <p>. Foi feito a importação da biblioteca random podendo assim gerar um número aleatorio</p>
+<p>função para gerar numero aleatorio</p>
+<p>random.randint gerar o numero e agrega na variavel numero_secreto</p>
     <div align="center">
     <img src="imgs/random.png" width="300">
     </div>
-
 <p>. introduzimos o loop While para que possamos conduzir as tentativas e a confirmação do acerto</p>
 <p>Pois a condição continuara enquanto não for verdadeiro(True)</p>
 
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import random 
-numero_secreto = random.randint(1 , 100) #função para gerar numero aleatorio
+numero_secreto = random.randint(1 , 100) 
 while True: #Aqui inicio o loop até que a condição seja verdadeira (Ou seja que jogador acerte o número secreto).
     chute = int(input("Digite um número entre 1 e 100: "))#O meu INPUT vai pedir o jogador para inserir um valor inteiro(str) no console a minha função INT convertera a string para número inteiro e guarda na variavel CHUTE
     if chute < numero_secreto:
